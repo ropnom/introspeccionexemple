319a4769acb7f50da9c0775fdde9a22bf5dde136
ropnom <ropnom5291@gmail.com>
2014-08-22 20:54:49 +0200

    fileds working

--- src/introspeccionexemple/Testintrospecion.java
+++ src/introspeccionexemple/Testintrospecion.java
@@ -10,43 +10,62 @@ public class Testintrospecion {
 
 		// creo el objeto
 		Objeto a = new Objeto(100, 222, 333);
+		// recordamso que el objeto tiene una variable publica y tres protected
 
+		System.out.println("Variable publica" + a.publica);
 		System.out.println("Metal:" + a.getMetal() + " Cristal:" + a.getCristal() + " Deuterio:" + a.getDeuterio());
 
+		// Iniciamos la introspeccion
+		System.out.println(" Iniciamos la introspeccion en busca de la variable publica...");
 		// aqui buscamos el campo directamente
 		Field field;
 		int valor = 0;
 		try {
 			field = Objeto.class.getDeclaredField("publica");
-			field.setAccessible(true);
-			System.out.println(field.getName() + " " + field.getGenericType());
+			System.out.println("Nombre de la variable: " + field.getName() + " Tipo:" + field.getGenericType() + " Valor:" + field.get(a));
+			System.out.println("------------------------------------------------------------------");
+			System.out.println(" Iniciamos la introspeccion en busca de la variable protected...");
+			field = Objeto.class.getDeclaredField("Metal");
+			field.setAccessible(true);// indicamos qeu se pueda acceder
+			System.out.println("Nombre de la variable: " + field.getName() + " Tipo:" + field.getGenericType() + " Valor:" + field.get(a));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		// MEtodos
+		System.out.println();
+		System.out.println("Ahroa printamos todas las variables del objeto:");
+		System.out.println();
+		// realizamos una introspeccion en busca del valor del metal.
+		Field[] fields = Objeto.class.getDeclaredFields();
+		System.out.println("Se han encontrado " + fields.length + " variables:");
+		// otra manera es obtener todas la variables
 		try {
-			Method m = Objeto.class.getMethod("GetMetal", null);
-			System.out.println(m);
-			//System.out.println(m.invoke(null, null));
-		} catch (NoSuchMethodException  e) {
+			for (Field f : fields) {
+				System.out.println("Nombre de la variable:  '" + f.getName() + "'  Tipo:" + f.getGenericType() + " Valor:" + f.get(a));
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		// realizamos una introspeccion en busca del valor del metal.
-		Field[] fields = Objeto.class.getDeclaredFields();
+		// // MEtodos
 
-		// Esto te devuelve
-		// las propiedades
-		// privadas de la clase
-		// Otra forma pero obteniendo todos los campos
-		// for (Field f : fields) {
-		// // realizamos un test por nombre de variable
-		// if (f.getName().equals("Metal")) {
-		// System.out.println("Introspeccion del metal:" + f);
-		// break;
-		// }
-		// }
+		try {
+			Method[] metodos = Objeto.class.getMethods();
+			for (Method f : metodos) {
+				System.out.println(f.getName() + " Types" + f.getTypeParameters());
+			}
+			// System.out.println(m.invoke(null, null));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+//		try {
+//			Method m = Objeto.class.getMethod("GetMetal", null);
+//			System.out.println(m.i);
+//			// System.out.println(m.invoke(null, null));
+//		} catch (NoSuchMethodException e) {
+//			e.printStackTrace();
+//		}
 
 	}
 }
